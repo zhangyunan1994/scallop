485cfa308b1e4f10d28333cfc2a08ccb58d6a2b2
zyndev <zyndev@gmail.com>
2018-03-28 22:51:07 +0800

    remove com.sparrow

--- src/main/java/com/dormouse/scallop/alibaba/client/entity/trade/NativeLogisticsInfo.java
+++ src/main/java/com/dormouse/scallop/alibaba/client/entity/trade/NativeLogisticsInfo.java
@@ -1,14 +1,18 @@
+/*
+ * Copyright (c) 2018. author and authors.
+ */
+
 package com.dormouse.scallop.alibaba.client.entity.trade;
 
 import java.io.Serializable;
 import java.util.List;
 
+
 /**
- * author: 张瑀楠
- * email : zyndev@gmail.com
- * desc  :
- * date  : 2017/8/30 下午9:24
- * todo  :
+ * The type Native logistics info.
+ *
+ * @author yunan.zhang
+ * @version 0.0.1
  */
 public class NativeLogisticsInfo implements Serializable{
 
@@ -28,9 +32,29 @@ public class NativeLogisticsInfo implements Serializable{
     private String townCode;//	java.lang.String	否	镇，街道地址码
     private String town;//	java.lang.String	否	镇，街道
 
+    /**
+     * Instantiates a new Native logistics info.
+     */
     public NativeLogisticsInfo() {
     }
 
+    /**
+     * Instantiates a new Native logistics info.
+     *
+     * @param address        the address
+     * @param area           the area
+     * @param areaCode       the area code
+     * @param city           the city
+     * @param contactPerson  the contact person
+     * @param fax            the fax
+     * @param mobile         the mobile
+     * @param province       the province
+     * @param telephone      the telephone
+     * @param zip            the zip
+     * @param logisticsItems the logistics items
+     * @param townCode       the town code
+     * @param town           the town
+     */
     public NativeLogisticsInfo(String address, String area, String areaCode, String city, String contactPerson, String fax, String mobile, String province, String telephone, String zip, List<NativeLogisticsItemsInfo> logisticsItems, String townCode, String town) {
         this.address = address;
         this.area = area;
@@ -66,106 +90,236 @@ public class NativeLogisticsInfo implements Serializable{
                 '}';
     }
 
+    /**
+     * Gets address.
+     *
+     * @return the address
+     */
     public String getAddress() {
         return address;
     }
 
+    /**
+     * Sets address.
+     *
+     * @param address the address
+     */
     public void setAddress(String address) {
         this.address = address;
     }
 
+    /**
+     * Gets area.
+     *
+     * @return the area
+     */
     public String getArea() {
         return area;
     }
 
+    /**
+     * Sets area.
+     *
+     * @param area the area
+     */
     public void setArea(String area) {
         this.area = area;
     }
 
+    /**
+     * Gets area code.
+     *
+     * @return the area code
+     */
     public String getAreaCode() {
         return areaCode;
     }
 
+    /**
+     * Sets area code.
+     *
+     * @param areaCode the area code
+     */
     public void setAreaCode(String areaCode) {
         this.areaCode = areaCode;
     }
 
+    /**
+     * Gets city.
+     *
+     * @return the city
+     */
     public String getCity() {
         return city;
     }
 
+    /**
+     * Sets city.
+     *
+     * @param city the city
+     */
     public void setCity(String city) {
         this.city = city;
     }
 
+    /**
+     * Gets contact person.
+     *
+     * @return the contact person
+     */
     public String getContactPerson() {
         return contactPerson;
     }
 
+    /**
+     * Sets contact person.
+     *
+     * @param contactPerson the contact person
+     */
     public void setContactPerson(String contactPerson) {
         this.contactPerson = contactPerson;
     }
 
+    /**
+     * Gets fax.
+     *
+     * @return the fax
+     */
     public String getFax() {
         return fax;
     }
 
+    /**
+     * Sets fax.
+     *
+     * @param fax the fax
+     */
     public void setFax(String fax) {
         this.fax = fax;
     }
 
+    /**
+     * Gets mobile.
+     *
+     * @return the mobile
+     */
     public String getMobile() {
         return mobile;
     }
 
+    /**
+     * Sets mobile.
+     *
+     * @param mobile the mobile
+     */
     public void setMobile(String mobile) {
         this.mobile = mobile;
     }
 
+    /**
+     * Gets province.
+     *
+     * @return the province
+     */
     public String getProvince() {
         return province;
     }
 
+    /**
+     * Sets province.
+     *
+     * @param province the province
+     */
     public void setProvince(String province) {
         this.province = province;
     }
 
+    /**
+     * Gets telephone.
+     *
+     * @return the telephone
+     */
     public String getTelephone() {
         return telephone;
     }
 
+    /**
+     * Sets telephone.
+     *
+     * @param telephone the telephone
+     */
     public void setTelephone(String telephone) {
         this.telephone = telephone;
     }
 
+    /**
+     * Gets zip.
+     *
+     * @return the zip
+     */
     public String getZip() {
         return zip;
     }
 
+    /**
+     * Sets zip.
+     *
+     * @param zip the zip
+     */
     public void setZip(String zip) {
         this.zip = zip;
     }
 
+    /**
+     * Gets logistics items.
+     *
+     * @return the logistics items
+     */
     public List<NativeLogisticsItemsInfo> getLogisticsItems() {
         return logisticsItems;
     }
 
+    /**
+     * Sets logistics items.
+     *
+     * @param logisticsItems the logistics items
+     */
     public void setLogisticsItems(List<NativeLogisticsItemsInfo> logisticsItems) {
         this.logisticsItems = logisticsItems;
     }
 
+    /**
+     * Gets town code.
+     *
+     * @return the town code
+     */
     public String getTownCode() {
         return townCode;
     }
 
+    /**
+     * Sets town code.
+     *
+     * @param townCode the town code
+     */
     public void setTownCode(String townCode) {
         this.townCode = townCode;
     }
 
+    /**
+     * Gets town.
+     *
+     * @return the town
+     */
     public String getTown() {
         return town;
     }
 
+    /**
+     * Sets town.
+     *
+     * @param town the town
+     */
     public void setTown(String town) {
         this.town = town;
     }
