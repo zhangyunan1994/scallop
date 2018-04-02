84e4ffbe723563c9026d8049d07a727f70c0eb31
zyndev <zyndev@gmail.com>
2018-04-02 18:19:06 +0800

    修改订单接口

--- src/main/java/com/dormouse/scallop/pinduoduo/api/OrderApi.java
+++ src/main/java/com/dormouse/scallop/pinduoduo/api/OrderApi.java
@@ -5,17 +5,68 @@
 package com.dormouse.scallop.pinduoduo.api;
 
 import com.dormouse.scallop.pinduoduo.domain.ApplicationParam;
+import com.dormouse.scallop.pinduoduo.domain.OrderInformation;
+
+import java.util.List;
 
 /**
+ * The type Order api.
+ *
  * @author 张瑀楠 zyndev@gmail.com
- * @version 1.0.0
- * 2018/3/31 下午1:25
+ * @version 0.0.1
  */
 public class OrderApi {
 
 
-    public void getInformation(ApplicationParam applicationParam, String accessToken, String orderSn) {
-
+    /**
+     * Gets information.
+     *
+     * @param applicationParam the application param
+     * @param accessToken      the access token
+     * @param orderSn          the order sn
+     * @return the information
+     */
+    public OrderInformation getInformation(ApplicationParam applicationParam, String accessToken, String orderSn) {
+        return null;
     }
 
+    /**
+     * Gets order list.
+     *
+     * @return the order list
+     */
+    public List<OrderInformation> getOrderList() {
+        return null;
+    }
+
+    /**
+     * Gets all order list.
+     * pdd.order.number.list.get（订单全量列表）
+     *
+     * @return the all order list
+     */
+    public List<OrderInformation> getAllOrderList() {
+        return null;
+    }
+
+    /**
+     * Gets increment order list.
+     * pdd.order.number.list.increment.get（订单增量接口）
+     *
+     * @return the increment order list
+     */
+    public List<OrderInformation> getIncrementOrderList() {
+        return null;
+    }
+
+
+    /**
+     * Gets order status.
+     * pdd.order.status.get（订单状态）
+     *
+     * @return the order status
+     */
+    public List<OrderInformation> getOrderStatus() {
+        return null;
+    }
 }
