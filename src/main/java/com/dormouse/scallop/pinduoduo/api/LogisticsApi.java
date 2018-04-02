ca9636dfd97d71cbd950e318f792bddf63756702
zyndev <zyndev@gmail.com>
2018-04-02 22:25:32 +0800

    add logistics info

--- src/main/java/com/dormouse/scallop/pinduoduo/api/LogisticsApi.java
+++ src/main/java/com/dormouse/scallop/pinduoduo/api/LogisticsApi.java
@@ -4,7 +4,12 @@
 
 package com.dormouse.scallop.pinduoduo.api;
 
-import com.dormouse.scallop.pinduoduo.domain.OrderInformation;
+
+import com.dormouse.scallop.pinduoduo.domain.Address;
+import com.dormouse.scallop.pinduoduo.domain.ApplicationParam;
+import com.dormouse.scallop.pinduoduo.domain.LogisticsCompany;
+
+import java.util.List;
 
 /**
  * @author 张瑀楠 zyndev@gmail.com
@@ -13,6 +18,17 @@ import com.dormouse.scallop.pinduoduo.domain.OrderInformation;
 public class LogisticsApi {
 
 
-    public OrderInformation getAddress()
+    public List<Address> getAddress(ApplicationParam applicationParam) {
+        return null;
+    }
+
+    public List<LogisticsCompany> getCompanies(ApplicationParam applicationParam) {
+        return null;
+    }
+
+    public void sendOnline(ApplicationParam applicationParam, String accessToken, String orderSn, String logisticsId,
+                           String trackingNumber) {
+
+    }
 
 }
