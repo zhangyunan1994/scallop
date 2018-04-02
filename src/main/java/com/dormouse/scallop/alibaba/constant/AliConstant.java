11efc62851b247276b36bd7f1e25d1ed3818cd48
zyndev <zyndev@gmail.com>
2018-04-02 17:17:36 +0800

    修改注释

--- src/main/java/com/dormouse/scallop/alibaba/constant/AliConstant.java
+++ src/main/java/com/dormouse/scallop/alibaba/constant/AliConstant.java
@@ -5,6 +5,8 @@
 package com.dormouse.scallop.alibaba.constant;
 
 /**
+ * The interface Ali constant.
+ *
  * @author 张瑀楠
  * @version 0.0.1
  */
@@ -15,6 +17,9 @@ public interface AliConstant {
      */
     String API_DOMAIN = "https://gw.open.1688.com/openapi/";
 
+    /**
+     * The constant API_PROTOCOL.
+     */
     String API_PROTOCOL = "param2/1/";
 
     /**
@@ -22,49 +27,69 @@ public interface AliConstant {
      */
     public static String WEBSITE="1688";
 
+    /**
+     * The interface Product.
+     */
     interface Product {
+        /**
+         * The constant NAMESPACE.
+         */
         String NAMESPACE = "com.alibaba.product/";
-        String PRODUCT_GETLIST = "alibaba.product.getList/"; // 获取商品列表（new）
-        String PRODUCT_GET = "alibaba.product.get/"; //由商品ID获取商品详细信息（new）
-        String PRODUCT_REPOST = "alibaba.product.repost/"; //商品重发（new）
-        String CATEGORY_GET = "alibaba.category.get/"; //商品重发（new）
-    }
-
-    interface Trade {
-        String NAMESPACE = "com.alibaba.trade/";
-        String GET_SELLER_ORDER_LIST = "alibaba.trade.getSellerOrderList/"; //获取卖家订单列表（new），不会返回订单的物流信息和发票信息；如果需要获取物流信息，请调用获取订单详情接口；
-    }
-
-    interface Logistics {
-        String SELLER_LOGISTICS_INFOS = "alibaba.trade.getLogisticsInfos.sellerView"; // 获取卖家的订单的物流详情
-        String SELLER_LOGISTICS_TRACEINFO = "alibaba.trade.getLogisticsTraceInfo.sellerView"; //获取卖家的订单的物流跟踪信息。
-        String SEND_ADDRESS_LIST = "alibaba.logistics.mySendGoodsAddress.list.get"; //我的发货地址列表
+        /**
+         * The constant PRODUCT_GETLIST.
+         * 获取商品列表（new）
+         */
+        String PRODUCT_GETLIST = "alibaba.product.getList/";
+        /**
+         * The constant PRODUCT_GET.
+         * 由商品ID获取商品详细信息（new）
+         */
+        String PRODUCT_GET = "alibaba.product.get/";
+        /**
+         * The constant PRODUCT_REPOST.
+         * 商品重发（new）
+         */
+        String PRODUCT_REPOST = "alibaba.product.repost/";
+        /**
+         * The constant CATEGORY_GET.
+         * 商品重发（new）
+         */
+        String CATEGORY_GET = "alibaba.category.get/";
     }
 
     /**
-     * 阿里巴巴常量
+     * The interface Trade.
      */
-    interface Alibaba {
-        //namespace: com.alibaba.product
-        //类目
-        String CATEGORY_ATTRIBUTE = "alibaba.category.attribute.get"; //获取类目属性
-        String CATEGORY_LIST = "alibaba.category.get"; //查询类目
+    interface Trade {
+        /**
+         * The constant NAMESPACE.
+         */
+        String NAMESPACE = "com.alibaba.trade/";
+        /**
+         * The constant GET_SELLER_ORDER_LIST.
+         * 获取卖家订单列表（new），不会返回订单的物流信息和发票信息；如果需要获取物流信息，请调用获取订单详情接口；
+         */
+        String GET_SELLER_ORDER_LIST = "alibaba.trade.getSellerOrderList/";
+    }
 
-        //产品（new）
-
-
-        //产品（old）
-        //namespace: cn.alibaba.open
-        String PRODUCT_GETLIST_OLD = "offer.getAllOfferList";  //获取当前会员的商品列表（old），不建议使用。
-        String PRODUCT_GET_OLD = "offer.get"; //获取单个商品（old），不建议使用。
-
-        String SHOWWINDOW_LIST = "industry.showwindow.doQueryRecommOfferList"; //获取卖家当前的橱窗列表
-        String SHOWWINDOW_DORECOMMEND = "industry.showwindow.doRecommendOffer"; //推荐一个产品为橱窗产品
-        String SHOWWINDOW_CANCEL = "industry.showwindow.cancelRecommendOffer"; //取消一个橱窗推荐产品
-
-
-        //物流（new）
-
-
+    /**
+     * The interface Logistics.
+     */
+    interface Logistics {
+        /**
+         * The constant SELLER_LOGISTICS_INFOS.
+         * 获取卖家的订单的物流详情
+         */
+        String SELLER_LOGISTICS_INFOS = "alibaba.trade.getLogisticsInfos.sellerView";
+        /**
+         * The constant SELLER_LOGISTICS_TRACEINFO.
+         * 获取卖家的订单的物流跟踪信息
+         */
+        String SELLER_LOGISTICS_TRACEINFO = "alibaba.trade.getLogisticsTraceInfo.sellerView";
+        /**
+         * The constant SEND_ADDRESS_LIST.
+         * 我的发货地址列表
+         */
+        String SEND_ADDRESS_LIST = "alibaba.logistics.mySendGoodsAddress.list.get";
     }
 }
--- src/main/java/com/dormouse/scallop/pinduoduo/constant/PddConstant.java
+++ src/main/java/com/dormouse/scallop/pinduoduo/constant/PddConstant.java
@@ -8,7 +8,7 @@ package com.dormouse.scallop.pinduoduo.constant;
  * The interface Pdd constant.
  *
  * @author 张瑀楠 zyndev@gmail.com
- * @version 1.0.0
+ * @version 0.0.1
  */
 public interface PddConstant {
 
@@ -53,68 +53,4 @@ public interface PddConstant {
         String STATUS_GET = "pdd.order.status.get";
     }
 
-    /**
-     * The interface Goods.
-     */
-    interface Goods {
-        /**
-         * The constant NAMESPACE.
-         */
-        String NAMESPACE = "com.alibaba.product/";
-        /**
-         * The constant PRODUCT_GETLIST.
-         * 获取商品列表（new）
-         */
-        String PRODUCT_GETLIST = "alibaba.product.getList/";
-        /**
-         * The constant PRODUCT_GET.
-         * 由商品ID获取商品详细信息（new）
-         */
-        String PRODUCT_GET = "alibaba.product.get/";
-        /**
-         * The constant PRODUCT_REPOST.
-         * 商品重发（new）
-         */
-        String PRODUCT_REPOST = "alibaba.product.repost/";
-        /**
-         * The constant CATEGORY_GET.
-         */
-        String CATEGORY_GET = "alibaba.category.get/";
-    }
-
-    /**
-     * The interface Trade.
-     */
-    interface Trade {
-        /**
-         * The constant NAMESPACE.
-         */
-        String NAMESPACE = "com.alibaba.trade/";
-        /**
-         * The constant GET_SELLER_ORDER_LIST.
-         * 获取卖家订单列表（new），不会返回订单的物流信息和发票信息；如果需要获取物流信息，请调用获取订单详情接口；
-         */
-        String GET_SELLER_ORDER_LIST = "alibaba.trade.getSellerOrderList/";
-    }
-
-    /**
-     * The interface Logistics.
-     */
-    interface Logistics {
-        /**
-         * The constant SELLER_LOGISTICS_INFOS.
-         * 获取卖家的订单的物流详情
-         */
-        String SELLER_LOGISTICS_INFOS = "alibaba.trade.getLogisticsInfos.sellerView";
-        /**
-         * The constant SELLER_LOGISTICS_TRACEINFO.
-         * 获取卖家的订单的物流跟踪信息
-         */
-        String SELLER_LOGISTICS_TRACEINFO = "alibaba.trade.getLogisticsTraceInfo.sellerView";
-        /**
-         * The constant SEND_ADDRESS_LIST.
-         * 我的发货地址列表
-         */
-        String SEND_ADDRESS_LIST = "alibaba.logistics.mySendGoodsAddress.list.get";
-    }
 }
